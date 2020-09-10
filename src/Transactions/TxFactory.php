4da3ee9158238dcda3ebaf5d5c0667bb9b5b2405
Furqan Siddiqui <hello@furqansiddiqui.com>
2020-09-10 20:36:21 +0500

    updates

--- src/Transactions/TxFactory.php
+++ src/Transactions/TxFactory.php
@@ -3,7 +3,9 @@ declare(strict_types=1);
 
 namespace SkyCoin\Transactions;
 
+use SkyCoin\Exception\SkyCoinTxException;
 use SkyCoin\SkyCoin;
+use SkyCoin\Wallets\PreparedTx;
 
 /**
  * Class TxFactory
@@ -23,7 +25,6 @@ class TxFactory
         $this->skyCoin = $sc;
     }
 
-
     /**
      * @param string $txId
      * @return Transaction
@@ -35,5 +36,32 @@ class TxFactory
     {
         $tx = $this->skyCoin->httpClient()->sendRequest(sprintf("/v1/transaction?txid=%s&verbose=1", $txId));
         return new Transaction($tx);
+    }
+
+    /**
+     * @param $encodedTx
+     * @return bool
+     * @throws SkyCoinTxException
+     * @throws \Comely\Http\Exception\HttpException
+     * @throws \SkyCoin\Exception\SkyCoinAPIException
+     */
+    public function inject($encodedTx): bool
+    {
+        if ($encodedTx instanceof PreparedTx) {
+            $encodedTx = $encodedTx->encodedTx;
+        }
+
+        if (!is_string($encodedTx) || !preg_match('/^[a-f0-9]+$/i', $encodedTx)) {
+            throw new SkyCoinTxException('Invalid encoded transaction; Cannot broadcast');
+        }
+
+        $this->skyCoin->httpClient()->sendRequest(
+            "/v1/injectTransaction",
+            ["rawtx" => $encodedTx],
+            [],
+            "POST"
+        );
+
+        return true;
     }
 }
